4bbbd5dcb64025acbe47519ab57edf43b29d4dc0
User <user@users-macbook.home>
2018-10-19 02:23:21 +0100

    started building out storyboard

---
+++ window-shopper/View/hourlywagetxt.swift
@@ -0,0 +1,27 @@
+//
+//  hourlywagetxt.swift
+//  window-shopper
+//
+//  Created by User on 19/10/2018.
+//  Copyright © 2018 User. All rights reserved.
+//
+
+import UIKit
+
+class hourlywagetxt: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 15
+        textAlignment = .center
+        
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        
+    }
+}
